51693e48fbd2320058240fc5e7cefd4e35dfe0be
Ekaterina Shpak <username@users.noreply.github.com>
2025-09-22 17:12:48 +0200

    Adjust buttons vissually

--- src/app/demo/[name]/ui/button.tsx
+++ src/app/demo/[name]/ui/button.tsx
@@ -3,8 +3,14 @@ import { Button } from "@/components/ui/button";
 export const button = {
   name: "button",
   components: {
-    Primary: <Button variant="default">Primary</Button>,
-    Secondary: <Button variant="secondary">Secondary</Button>,
-    Outline: <Button variant="outline">Outline</Button>,
+    Emphasized: <Button variant="default">Emphasized</Button>,
+    // Primary1: <Button variant="primary">Primary</Button>,
+    Default: <Button variant="outline">Default</Button>,
+    Transparent: <Button variant="ghost">Transparent</Button>,
+    Negative: <Button variant="destructive">Negative</Button>,
+    Positive: <Button variant="positive">Positive</Button>,
+    // Ghost: <Button variant="ghost">Ghost</Button>,
+    // Ghost: <Button variant="ghost">Ghost</Button>,
+    // Link: <Button variant="link">Link</Button>,
   },
 };
